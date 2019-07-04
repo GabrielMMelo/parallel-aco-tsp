4cdbcc292923d484a8913c96569dfe5a1e06cb82
Gabriel Marques de Melo <gabrielmelocomp@gmail.com>
2019-07-04 06:36:41 -0300

    Added Docstrings!

--- aco_parallel.py
+++ aco_parallel.py
@@ -7,19 +7,19 @@ class Grafo(object):
     def __init__(self, matriz_adjacencia, rank):
         self.matriz = matriz_adjacencia
         self.rank = rank
-        self.feronomio = [[1 / (rank * rank) for j in range(rank)]
+        self.feromonio = [[1 / (rank * rank) for j in range(rank)]
                           for i in range(rank)]  # m x m
 
 
 class ACO(object):
     def __init__(self, cont_formiga, geracoes, alfa, beta, ro, Q):
         """
-        Q            = intensidade do feromonio
-        ro           = taxa de evaporação
-        beta         = influencia da visibilidade (proximidade entre os nohs)
-        alfa         = influencia do feromonio
-        cont_formiga = m
-        geracoes     = iterações do algoritmo
+        Q -- intensidade do feromonio (default=0.0)
+        ro -- taxa de evaporação
+        beta -- influencia da visibilidade (proximidade entre os nohs)
+        alfa -- influencia do feromonio
+        cont_formiga -- m
+        geracoes -- iterações do algoritmo
         """
         self.Q = Q
         self.ro = ro
@@ -28,15 +28,25 @@ class ACO(object):
         self.cont_formiga = cont_formiga
         self.geracoes = geracoes
 
-    # OK
-    def _atualiza_feronomio(self, grafo, formigas):
-        for i, linha in enumerate(grafo.feronomio):
+    def _atualiza_feromonio(self, grafo, formigas):
+        """Atualização dos feromônios globais (entre cada cidade)
+
+        Keyword arguments:
+        grafo -- instância de Grafo
+        formigas -- vetor das formigas da colônia
+        """
+        for i, linha in enumerate(grafo.feromonio):
             for j, coluna in enumerate(linha):
-                grafo.feronomio[i][j] *= self.ro  # evaporacao
+                grafo.feromonio[i][j] *= self.ro  # evaporacao
                 for formiga in formigas:
-                    grafo.feronomio[i][j] += formiga.feronomio_delta[i][j]
-    # ok
+                    grafo.feromonio[i][j] += formiga.feromonio_delta[i][j]
+
     def resolve(self, grafo):
+        """Execução do ACO
+
+        Keyword arguments:
+        grafo -- instância de Grafo
+        """
         melhor_custo = float('inf')
         melhor_solucao = []
         for gen in range(self.geracoes):
@@ -55,13 +65,12 @@ class ACO(object):
             for formiga in formigas:
                 for j in range(grafo.rank - 1):
                     formiga._seleciona_proximo()
-                formiga.custo_total += grafo.matriz[formiga.tabu[-1]][  # Retorno para o noh inicial
+                formiga.custo_total += grafo.matriz[formiga.tabu[-1]][  # retorno para cidade inicial
                     formiga.tabu[0]]
                 if formiga.custo_total < melhor_custo:
                     melhor_custo = formiga.custo_total
                     melhor_solucao = [] + formiga.tabu
-                # atualiza feronomio
-                formiga._atualiza_feronomio_delta()
+                formiga._atualiza_feromonio_delta()  # atualiza feromonio
             #print(melhor_custo)
             if _rank:
                 comm.send(formigas, dest=0, tag=1)
@@ -69,19 +78,23 @@ class ACO(object):
                 for i in range(1, size):
                     for formiga in comm.recv(source=i, tag=1):
                         formigas.append(formiga)
-                self._atualiza_feronomio(grafo, formigas)
-            # print('generation #{}, best cost: {}, path: {}'.format(gen, melhor_custo, melhor_solucao))
+                self._atualiza_feromonio(grafo, formigas)
         if not _rank:
             return melhor_solucao, melhor_custo
 
 
 class _Formiga(object):
     def __init__(self, aco, grafo):
+        """
+        Keyword arguments:
+        aco -- instância de ACO
+        grafo -- instância de Grafo
+        """
         self.colonia = aco
         self.grafo = grafo
         self.custo_total = 0.0  # Lk
         self.tabu = []  # caminho escolhido pela formiga em uma geração
-        self.feronomio_delta = []  #deltaT^Kij
+        self.feromonio_delta = []  #deltaT^Kij
         self.permitido = [i for i in range(grafo.rank)]
         self.eta = [[  # 1/Lij
             0 if i == j else 1 / grafo.matriz[i][j] for j in range(grafo.rank)
@@ -91,25 +104,28 @@ class _Formiga(object):
         self.atual = inicio
         self.permitido.remove(inicio)
 
-    # OK
     def _seleciona_proximo(self):
+        """Seleciona próxima cidade de uma formiga através do cálculo das
+        probabilidades de aceder cada uma das demais. Utiliza técnica de
+        Roullete Wheel para selecionar.
+        """
         denominador = 0
         for j in self.permitido:
-            denominador += self.grafo.feronomio[
+            denominador += self.grafo.feromonio[
                 self.atual][j]**self.colonia.alfa * self.eta[
                     self.atual][j]**self.colonia.beta
         probabilidades = [
             0 for i in range(self.grafo.rank)
-        ]  # probabilidades de mover para um nó no próximo passo
+        ]  # probabilidades de mover para uma cidade no próximo passo
         for i in range(self.grafo.rank):
             try:
                 self.permitido.index(i)  # checa se o noh eh permitido
-                probabilidades[i] = self.grafo.feronomio[self.atual][i] ** self.colonia.alfa * \
+                probabilidades[i] = self.grafo.feromonio[self.atual][i] ** self.colonia.alfa * \
                     self.eta[self.atual][i] ** self.colonia.beta / denominador
             except ValueError:
-                pass  # descarta
+                pass   # descarta se a cidade nao for permitida
 
-        # Seleciona o proximo noh usando a técnica de roulette wheel
+        # seleciona a proxima cidade usando a técnica de roulette wheel
         selecionado = 0
         rand = random.random()
         for i, probabilidade in enumerate(probabilidades):
@@ -122,10 +138,11 @@ class _Formiga(object):
         self.custo_total += self.grafo.matriz[self.atual][selecionado]
         self.atual = selecionado
 
-    def _atualiza_feronomio_delta(self):
-        self.feronomio_delta = [[0 for j in range(self.grafo.rank)]  # zera deltaT^kij
+    def _atualiza_feromonio_delta(self):
+        """Atualiza os feromônios locais de uma formiga"""
+        self.feromonio_delta = [[0 for j in range(self.grafo.rank)]  # zera deltaT^kij
                                 for i in range(self.grafo.rank)]
         for _ in range(1, len(self.tabu)):
             i = self.tabu[_ - 1]
             j = self.tabu[_]
-            self.feronomio_delta[i][j] = self.colonia.Q / self.custo_total
+            self.feromonio_delta[i][j] = self.colonia.Q / self.custo_total
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import math
 import sys
 import argparse
@@ -18,6 +19,12 @@ else:
     from aco import ACO, Grafo
 
 def calc_distancia(cidade1, cidade2):
+    """Calcula distância entre duas cidades
+
+    Keyword arguments:
+    cidade1 -- dict com coordenadas [x,y] da cidade
+    cidade2 -- dict com coordenadas [x,y] da cidade
+    """
     return math.sqrt((cidade1['x'] - cidade2['x'])**2 +
                      (cidade1['y'] - cidade2['y'])**2)
 
@@ -25,19 +32,22 @@ def calc_distancia(cidade1, cidade2):
 def main():
     cidades = []
     pontos = []
-    with open('./data/chn31.txt') as f:
+
+    with open('./data/chn31.txt') as f:  # leitura das cidades
         for linha in f.readlines():
             cidade = linha.split(' ')
             cidades.append(
                 dict(index=int(cidade[0]), x=int(cidade[1]), y=int(cidade[2])))
             pontos.append((int(cidade[1]), int(cidade[2])))
-    matriz_adjacencia = []  # matriz de adjacencia
+
+    matriz_adjacencia = []
     rank = len(cidades)
-    for i in range(rank):
+    for i in range(rank):  # calculo da matriz de adjacencia
         linha = []
         for j in range(rank):
             linha.append(calc_distancia(cidades[i], cidades[j]))
         matriz_adjacencia.append(linha)
+
     aco = ACO(cont_formiga=100, geracoes=100, alfa=1.0, beta=10.0, ro=0.5, Q=10)
     grafo = Grafo(matriz_adjacencia, rank)
     try:
